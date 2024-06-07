b5b2c1f9cfb6466e2705b83ea1dfe47d625dafb7
JiHwan Kim <57897955+doram419@users.noreply.github.com>
2024-06-07 13:12:47 +0900

    13:12

--- src/main/java/himedia/filter/EncodingFilter.java
+++ src/main/java/himedia/filter/EncodingFilter.java
@@ -1,6 +1,7 @@
 package himedia.filter;
 
 import java.io.IOException;
+import java.util.logging.Logger;
 
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
@@ -9,10 +10,13 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 
-public class EncordingFilter implements Filter{
+public class EncodingFilter implements Filter{
+	private static Logger logger = Logger.getLogger(EncodingFilter.class.getSimpleName());	//로거 선언
+	private static String encoding = "UTF-8";
 	
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
+		logger.info("[Encoding Filter] init");
 		Filter.super.init(filterConfig);
 	}
 	
@@ -21,12 +25,14 @@ public class EncordingFilter implements Filter{
 			ServletResponse resp, 
 			FilterChain chain)
 			throws IOException, ServletException {
+		logger.info("[Encoding Filter] doFilter");
 		
 	}
 
 	@Override
 	public void destroy() {
 		Filter.super.destroy();
+		logger.info("[Encoding Filter] destroy");
 	}
 
 
