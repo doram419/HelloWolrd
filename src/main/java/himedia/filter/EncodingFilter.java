40184a7645e1a219d69e1a427ea3a93d5effb79e
JiHwan Kim <57897955+doram419@users.noreply.github.com>
2024-06-07 14:30:05 +0900

    14:30

--- src/main/java/himedia/filter/EncodingFilter.java
+++ src/main/java/himedia/filter/EncodingFilter.java
@@ -1,6 +1,7 @@
 package himedia.filter;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.logging.Logger;
 
 import jakarta.servlet.Filter;
@@ -25,13 +26,25 @@ public class EncodingFilter implements Filter{
 			ServletResponse resp, 
 			FilterChain chain)
 			throws IOException, ServletException {
-		logger.info("[Encoding Filter] doFilter");
 		
+		// 출력을 위한 객체 얻어오기
+		PrintWriter out = resp.getWriter();
+		out.println("<h6>Encoding Filter 작동 전</h6>");
+		
+		// 실제 필터가 처리하는 내용
+		logger.info("[Encoding Filter] doFilter");
+		req.setCharacterEncoding(encoding);	// 요청 정보의 인코딩 설정 : 요즘은 많이 좋아져서 깨지는 경우는 적지만 해주기
+		resp.setContentType("text/html");		// 응답 정보의 타입
+		resp.setCharacterEncoding(encoding);	// 응답 정보의 인코딩
+		
+		chain.doFilter(req, resp);	// 요청 처리 종료 후 다음 필터로 전달해준다. 이게 핵심
+		
+		out.println("<h6>Encoding Filter 작동 후</h6>");
 	}
 
 	@Override
 	public void destroy() {
-		Filter.super.destroy();
+//		Filter.super.destroy();
 		logger.info("[Encoding Filter] destroy");
 	}
 
