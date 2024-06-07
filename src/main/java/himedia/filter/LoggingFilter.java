5832a2f7bbf211d41f3db6022dbef21a38a16081
JiHwan Kim <57897955+doram419@users.noreply.github.com>
2024-06-07 17:15:32 +0900

    friday

--- src/main/java/himedia/filter/LoggingFilter.java
+++ src/main/java/himedia/filter/LoggingFilter.java
@@ -1,6 +1,8 @@
 package himedia.filter;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Logger;
 
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
@@ -8,22 +10,42 @@ import jakarta.servlet.FilterConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
 
+// 요청과 응답의 기본적 내용을 기록으로 남기는 필터
 public class LoggingFilter implements Filter {
-
+	private static Logger logger = Logger.getLogger(LoggingFilter.class.getSimpleName());
+	
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
+		logger.info("[LogginFilter] init");
 		Filter.super.init(filterConfig);
 	}
 	
 	@Override
-	public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain arg2)
+	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
 			throws IOException, ServletException {
-
+		logger.info("[LogginFilter] doFilter");
+		PrintWriter out = resp.getWriter();
+		out.println("<h6>LoggingFilter 작동 전</h6>");
+		
+		// 요청 정보를 로그로 출력
+		// ServletRequest -> HttpServletRequest 캐스팅 URI 정보
+		HttpServletRequest httpRequest = (HttpServletRequest) req;
+		logger.info("들어오는 요청의 URI: " + httpRequest.getRequestURI());
+		
+		// 필터 로직을 수행한 후 연결된 필터(혹은 서블릿)으로 처리 중인 요청과 응답 객체 전달
+		// 이게 핵심
+		chain.doFilter(req, resp);
+		
+		// 응답이 돌아온 이후 응답 상태를 출력하고 로깅
+		out.println("<h6>LoggingFilter 작동 후</h6>");
+		logger.info("출력되는 응답의 상태: " + resp.getContentType());
 	}
 
 	@Override
 	public void destroy() {
+		logger.info("[LogginFilter] destroy");
 		Filter.super.destroy();
 	}
 }
